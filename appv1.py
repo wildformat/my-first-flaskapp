0883578dd966722e03d67e7e618b307530bf3d96
elizabeth <wangarithuo@gmail.com>
2022-01-27 14:16:07 +0300

    feat: create multiple versions of app

--- appv1.py
+++ appv1.py
@@ -2,4 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/") #location of service implemented by my_function
 def my_function():
-    return("hello Liz")
+    return("hello Liz, from v1")
