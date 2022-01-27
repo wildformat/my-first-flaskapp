a5e34d77aa233a4af9a75e38144694ba59b0b153
elizabeth <wangarithuo@gmail.com>
2022-01-27 15:38:13 +0300

    feat: call app_v2 from app_v1

--- appv1.py
+++ appv1.py
@@ -1,5 +1,13 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 @app.route("/") #location of service implemented by my_function
 def my_function():
     return("hello Liz, from v1")
+
+@app.route("/call")
+def call():
+    req = requests.get(url="http://app_v2:5000/")
+    return(req.text)
+
+    
